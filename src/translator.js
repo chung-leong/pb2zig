d2a706c90d4f1f5059b45d5863c11112312dd8f5
Chung Leong <chernyshevsky@hotmail.com>
2023-09-17 22:42:08 +0200

    Reworked indentation

--- src/translator.js
+++ src/translator.js
@@ -1,161 +1,211 @@
 import * as N from './nodes.js';
 
 export class PixelBenderToZigTranslator {
-    lines = [ '' ];
-    tabs = 0;
-    ast = null;
+  lines = [ '' ];
+  indent = 0;
+  ast = null;
 
-    indent(delta) {
-        this.tabs += delta;
-    }
-
-    add(text) {
-        var index = this.lines.length - 1;
-        var line = this.lines[index];
-        if (line.endsWith('{')) {
-            if (!text.startsWith('}'))  {
-                this.nl();
-                line = '';
-                index++;
-            }
-            this.indent(+1);
+  add(text) {
+    const newLines = text.trim().split('\n').map(l => l.trim());
+    for (const line of newLines) {
+      let indent = this.indent;
+      if (line.startsWith('}')) {
+        indent--;
+      }
+      const spaces = ' '.repeat(Math.max(0, indent) * 4);
+      this.lines.push(spaces + line);
+      for (const c of line) {
+        if (c === '{') {
+          this.indent++;
+        } else if (c === '}') {
+          this.indent--;
         }
-        if (text.startsWith('}')) {
-            this.indent(-1);
+      }
+    }
+  }
+
+  find(Class) {
+    const list = [];
+    const f = (node) => {
+      if (node instanceof Array) {
+        node.forEach(f);
+      } else if (node instanceof Class) {
+        list.push(node);
+      } else if (node instanceof Object) {
+        for (const n of Object.values(node)) {
+          f(n);
         }
-        if (!line.endsWith('{')) {
-            line += ' '.repeat(!line ? this.tabs * 4 : 1);
+      }
+    };
+    f(this.ast);
+    return list;
+  }
+
+  translate(ast) {
+    this.ast = ast;
+    this.addMetadata();
+    this.addKernel();
+    this.ast = null;
+    return this.lines;
+  }
+
+  type(type) {
+    const table = {
+      bool: 'bool',
+      bool2: 'bool[2]',
+      bool3: 'bool[3]',
+      bool4: 'bool[4]',
+
+      int: 'i32',
+      int2: '@Vector(2, i32)',
+      int3: '@Vector(3, i32)',
+      int4: '@Vector(4, i32)',
+
+      float: 'f32',
+      float2: '@Vector(2, f32)',
+      float3: '@Vector(3, f32)',
+      float4: '@Vector(4, f32)',
+    };
+    const zigType = table[type];
+    if (!zigType) {
+      throw new Error(`Unknown type: ${type}`);
+    }
+    return zigType;
+  }
+
+  value(value, type) {
+    if (type === 'float') {
+      let s = value.toString();
+      if (s.indexOf('.') === -1) {
+        s = value.toFixed(1);
+      }
+      return s;
+    } else {
+      return JSON.stringify(value);
+    }
+  }
+
+  addMetadata() {
+    const { name, meta } = this.ast;
+    this.add(`// Pixel Bender "${name}" (translated using pb2zig)`);
+    for (const [ field, value ] of Object.entries(meta)) {
+      if (value) {
+        this.add(`// ${field}: ${value}`);
+      }
+    }
+    this.add(``);
+  }
+
+  addKernel() {
+    this.add(`pub const kernel = struct {`);
+    //this.addParameterFields();
+    this.addParameterDecls();
+    this.addInput();
+    this.addOutput();
+    this.add(``);
+    this.addInstanceFunction();
+    this.add(``);
+    this.addCreateFunction();
+    this.add(`};`);
+  }
+
+  addParameterDecls() {
+    const params = this.find(N.Parameter);
+    this.add(`const parameters = .{`);
+    for (const param of params) {
+      const {
+        name,
+        type,
+        minValue,
+        maxValue,
+        stepInterval,
+        defaultValue,
+        previewValue,
+        ...others
+      } = param;
+      this.add(`.${param.name} = .{`);
+      this.add(`.type = ${this.type(type)},`);
+      if (minValue !== undefined) {
+        this.add(`.minValue = ${this.value(minValue, type)}`);
+      }
+      if (maxValue !== undefined) {
+        this.add(`.maxValue = ${this.value(maxValue, type)}`);
+      }
+      if (stepInterval !== undefined) {
+        this.add(`.stepInterval = ${this.value(stepInterval, type)}`);
+      }
+      if (defaultValue !== undefined) {
+        this.add(`.defaultValue = ${this.value(defaultValue, type)}`);
+      }
+      if (previewValue !== undefined) {
+        this.add(`.previewValue = ${this.value(previewValue, type)}`);
+      }
+      for (const [ name, value ] of Object.entries(others)) {
+        this.add(`.${name} = ${this.value(value, type)},`);
+      }
+      this.add(`},`);
+    }
+    this.add(`};`);
+  }
+
+  addInput() {
+    const inputs = this.find(N.InputDeclaration);
+    this.add(`pub const input = .{`);
+    for (const { name, type } of inputs) {
+      const channels = parseInt(type.replace(/\D/g, ''));
+      this.add(`${name} = .{ channels: ${channels} },`);
+    }
+    this.add('};');
+  }
+
+  addOutput() {
+    const outputs = this.find(N.OutputDeclaration);
+    this.add(`pub const output = .{`);
+    for (const { name, type } of outputs) {
+      const channels = parseInt(type.replace(/\D/g, ''));
+      this.add(`${name} = .{ channels: ${channels} },`);
+    }
+    this.add('};');
+  }
+
+  addInstanceFunction() {
+    this.add(`fn Instance(comptime InputStruct: type) type {`);
+    this.addParameterFields();
+    this.addInputFields();
+    this.add(``);
+    this.addCalledFunctions();
+    this.add(`}`);
+  }
+
+  addParameterFields() {
+    const params = this.find(N.Parameter);
+    for (const param of params) {
+      const type = this.type(param.type);
+      this.add(`${param.name}: ${type},`);
+    }
+  }
+
+  addInputFields() {
+    const inputs = this.find(N.InputDeclaration);
+    for (const { name } of inputs) {
+     this.add(`${name} = std.meta.fieldInfo(InputStruct, .src).type,`);
+    }
+  }
+
+  addCalledFunctions() {
+    const calls = this.find(N.FunctionCall);
+    console.log(calls);
+  }
+
+  addCreateFunction() {
+    this.add(`
+      fn create(inputStruct: anytype) Instance(@TypeOf(inputStruct)) {
+        var instance: Instance(@TypeOf(inputStruct)) = undefined;
+        inline for (std.meta.fields(@TypeOf(inputStruct))) |field| {
+            @field(instance, field.name) = @field(inputStruct, field.name);
         }
-        line += text;
-        this.lines[index] = line;
-        if (text.startsWith('}')) {
-            this.nl();
-        }
-    }
-
-    nl(count = 1) {
-        for (let i = 0; i < count; i++) {
-            this.lines.push('');
-        }
-    }
-
-    find(Class) {
-        const list = [];
-        const f = (node) => {
-            if (node instanceof Array) {
-                node.forEach(f);
-            } else if (node instanceof Class) {
-                list.push(node);
-            } else if (node instanceof Object) {
-                for (const n of Object.values(node)) {
-                    f(n);
-                }
-            }
-        };
-        f(this.ast);
-        return list;
-    }
-
-    translate(ast) {
-        this.ast = ast;
-        this.addMetadata();
-        this.addTypeFunction();
-        this.ast = null;
-        return this.lines;
-    }
-
-    translateType(type) {
-        const table = {
-            bool: 'bool',
-            bool2: 'bool[2]',
-            bool3: 'bool[3]',
-            bool4: 'bool[4]',
-
-            int: 'i32',
-            int2: '@Vector(2, i32)',
-            int3: '@Vector(3, i32)',
-            int4: '@Vector(4, i32)',
-
-            float: 'f32',
-            float2: '@Vector(2, f32)',
-            float3: '@Vector(3, f32)',
-            float4: '@Vector(4, f32)',
-        };
-        const zigType = table[type];
-        if (!zigType) {
-            throw new Error(`Unknown type: ${type}`);
-        }
-        return zigType;
-    }
-
-    addMetadata() {
-        const { name, meta } = this.ast;
-        this.add(`// Pixel Bender "${name}" (translated using pb2zig)`);
-        this.nl(2);
-        for (const [ field, value ] of Object.entries(meta)) {
-            if (value) {
-                this.add(`// ${field}: ${value}`);
-                this.nl();
-            }
-        }
-        this.nl();
-    }
-
-    addTypeFunction() {
-        this.add(`pub fn Kernel(comptime Image: type, comptime sampler: anytype) type {`);
-        this.add(`return struct {`);
-        this.addParameterFields();
-        this.addParameterDecls();
-        this.addSources();
-        this.addDestination();
-        this.addPixelFunction();
-        this.add(`};`);
-        this.add(`}`);
-    }
-
-    addParameterFields() {
-        const params = this.find(N.Parameter);
-        this.add(`// parameter fields`)
-        this.nl();
-        for (const param of params) {
-            const type = this.translateType(param.type);
-            if (param.defaultValue != undefined) {
-                this.add(`var ${param.name}:${type} = parameters.${param.name}.defaultValue;`);
-            } else {
-                this.add(`var ${param.name}:${type};`);
-            }
-            this.nl();
-        }
-        this.nl();
-    }
-
-    addParameterDecls() {
-        const params = this.find(N.Parameter);
-        this.add(`// parameter info`);
-        this.nl();
-        this.add(`const parameters = .{`);
-        for (const param of params) {
-            this.add(`.${param.name} = .{`);
-            for (const [ name, value ] of Object.entries(param)) {
-                if (name !== 'name' && name !== 'type' && value != undefined) {
-                    this.add(`.${name} = ${JSON.stringify(value)},`);
-                    this.nl();
-                }
-            }
-            this.add(`},`);
-        }
-        this.add(`};`);
-        this.nl();
-    }
-
-    addSources() {
-    }
-
-    addDestination() {
-
-    }
-
-    addPixelFunction() {
-
-    }
+        return instance;
+      }
+    `.trim());
+  }
 }
