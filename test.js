c5873c5cdc64e0be7b915566d427574d505e5618
Chung Leong <chernyshevsky@hotmail.com>
2023-09-18 12:14:02 +0200

    Added sample PBK files

--- test.js
+++ test.js
@@ -3,21 +3,21 @@ import { parse } from './src/parser.js';
 import { PixelBenderAstVisitor } from './src/visitor.js';
 import { PixelBenderToZigTranslator } from './src/translator.js';
 
-const text = readFileSync('./painting.pbk', 'utf8');
+const text = readFileSync('./test/pbk-samples/painting.pbk', 'utf8');
 const { cst, lexErrors, parseErrors } = parse(text);
 
 if (lexErrors.length > 0) {
   console.log('Errors encountered by lexer:');
   for (const error of lexErrors) {
     console.log(error);
-  }    
+  }
 }
 
 if (parseErrors.length > 0) {
   console.log('Errors encountered by parser:');
   for (const error of parseErrors) {
     console.log(error);
-  }    
+  }
 }
 
 const visitor = new PixelBenderAstVisitor();
