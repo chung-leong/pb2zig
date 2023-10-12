ffa2973cb460a3e19b4c7de6c24851941b267e01
Chung Leong <chernyshevsky@hotmail.com>
2023-10-12 20:29:42 +0200

    Improved worker management (#17)

--- rollup-plugin-pb2zig/dist/manager.js
+++ rollup-plugin-pb2zig/dist/manager.js
@@ -1,12 +1,12 @@
-export async function getKernelInfo(workerNum = 0) {
-  return startJob(workerNum, 'getKernelInfo');
+export async function getKernelInfo() {
+  return startJob('getKernelInfo');
 }
 
-export async function createImageData(width, height, source = {}, params = {}, workerNum = 0) {
-  return createPartialImageData(width, height, 0, height, source, params, workerNum);
+export async function createImageData(width, height, source = {}, params = {}) {
+  return createPartialImageData(width, height, 0, height, source, params);
 }
 
-export async function createPartialImageData(width, height, start, count, source = {}, params = {}, workerNum = 0) {
+export async function createPartialImageData(width, height, start, count, source = {}, params = {}) {
   const transfer = [];
   const args = [ width, height, start, count, source, params ];
   if ('data' in source && 'width' in source && 'height' in source) {
@@ -16,33 +16,64 @@ export async function createPartialImageData(width, height, start, count, source
       transfer.push(source.data.buffer);
     }
   }
-  return startJob(workerNum, 'createPartialImageData', args, transfer);
+  return startJob('createPartialImageData', args, transfer);
 }
 
-export async function releaseWorkers() {
-  const promises = jobs.map(j => j.promise);
-  await Promise.allSettled(promises);
-  workers.splice(0);
+export function manageWorkers(settings) {
+  keepAlive = settings.keepAlive;
+  maxCount = settings.maxCount;
+  if (!keepAlive) {
+    idleWorkers.splice(0);
+  } else {
+    const extra = (idleWorkers.length + activeWorkers.length) - maxCount;
+    if (extra > 0) {
+      idleWorkers.splice(0, extra);
+    }
+  }
 }
 
 const workerURL = '[WORKER-URL]';
-const workers = [];
 
+let keepAlive = true;
+let maxCount = navigator.hardwareConcurrency;
+
+const activeWorkers = [];
+const idleWorkers = [];
+const pendingRequests = [];
 const jobs = [];
+
 let nextJobID = 1;
 
-function startJob(workerNum, name, args = [], transfer = []) {
-  let worker = workers[workerNum];
+async function acquireWorker() {
+  let worker = idleWorkers.shift();
   if (!worker) {
-    worker = workers[workerNum] = new Worker(workerURL, { type: 'module' });
-    worker.onmessage = handleMessage;
-    worker.onerror = (evt) => console.error(evt);
+    if (maxCount < 1) {
+      throw new Error(`Unable to start worker because maxCount is ${maxCount}`);
+    }
+    if (activeWorkers.length < maxCount) {
+      // start a new one
+      worker = new Worker(workerURL, { type: 'module' });
+      worker.onmessage = handleMessage;
+      worker.onerror = (evt) => console.error(evt);
+    } else {
+      // wait for the next worker to become available again
+      return new Promise((resolve) => {
+        pendingRequests.push(resolve);
+      });
+    }
   }
+  activeWorkers.push(worker);
+  return worker;
+}
+
+async function startJob(name, args = [], transfer = []) {
+  const worker = await acquireWorker();
   const job = {
     id: nextJobID++,
     promise: null,
     resolve: null,
     reject: null,
+    worker,
   };
   job.promise = new Promise((resolve, reject) => {
     job.resolve = resolve;
@@ -58,9 +89,23 @@ function handleMessage(evt) {
   const jobIndex = jobs.findIndex(j => j.id === jobID);
   const job = jobs[jobIndex];
   jobs.splice(jobIndex, 1);
+  const { worker, resolve, reject } = job;
   if (name !== 'error') {
-    job.resolve(result);
+    resolve(result);
   } else {
-    job.reject(result);
+    reject(result);
+  }
+  // work on pending request if any
+  const next = pendingRequests.shift();
+  if (next) {
+    next(worker);
+  } else {
+    const workerIndex = activeWorkers.indexOf(worker);
+    if (workerIndex !== -1) {
+      activeWorkers.splice(workerIndex, 1);
+    }
+    if (keepAlive && idleWorkers.length < maxCount) {
+      idleWorkers.push(worker);
+    }
   }
 }
